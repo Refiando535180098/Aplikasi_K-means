ec3bfbbda026445248c9651d6a5b4044f5d80fd3
Refiando535180098 <refiando.535180098@stu.untar.ac.id>
2023-01-06 23:05:12 +0700

    Update Proses.py

--- final/pages/Proses.py
+++ final/pages/Proses.py
@@ -142,7 +142,7 @@ if selected == "Data set input user":
         st.write("**File yang diinput harus bertype csv dan berformat seperti gambar dibawah ini :**") 
         st.image("/app/aplikasi_k-means/final/image/datatabel.png", width=500)
         st.write("**Catatan :**")
-        st.write("**Agar program dapat membaca file format kolom harus dengan Kolom 1, Kolom 2, dan Kolom 3**")        
+        st.write("**Agar program dapat membaca file, format kolom harus dengan Kolom 1, Kolom 2, dan Kolom 3**")        
         file = st.file_uploader("**Upload file**", type=["csv"])
         show_file = st.empty()
     
