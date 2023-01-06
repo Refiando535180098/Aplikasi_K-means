f06ef4e0587550451f801da8b80427cde7277043
Refiando535180098 <refiando.535180098@stu.untar.ac.id>
2023-01-06 22:23:45 +0700

    Update Proses.py

--- final/pages/Proses.py
+++ final/pages/Proses.py
@@ -38,22 +38,22 @@ st.markdown(markdown, unsafe_allow_html=True)
 
 st.title("Proses K-Means")
 st.write("""
-            Dalam pengelompokan menggunakan K-means dibutuhkan penentuan jumlah
+            **Dalam pengelompokan menggunakan K-means dibutuhkan penentuan jumlah
             klaster yang akan menjadi penentu data mana yang merupakan anggota
-            dari klaster.
+            dari klaster.**
             """)
 
 nilai = st.slider('**Input klaster :**', 0, 3, 6)
 
-tab1, tab2 = st.tabs(["Data set olah", "Data set input user"])
+tab1, tab2 = st.tabs(["**Data set olah**", "**Data set input user**"])
 
 with tab1:
     st.title("Dataset yang diolah :")
     st.write("""
-            Tabel berikut ini merupakan tabel akumulasi data responden dari
-            data set Kesulitan, Durasi dan Cerita.
+            **Tabel berikut ini merupakan tabel akumulasi data responden dari
+            data set Kesulitan, Durasi dan Cerita.**
             """)
-    st.write("Dikarenakan data berupa array maka dimulai dari data ke 0, maka data 0 diartikan sebagai data ke 1 dan seterusnya.")
+    st.write("**Dikarenakan data berupa array maka dimulai dari data ke 0, maka data 0 diartikan sebagai data ke 1 dan seterusnya.**")
 
     #query = "SELECT `kesulitan`, `durasi`, `cerita` FROM data_olah"
     #mycursor.execute(query)
@@ -62,23 +62,23 @@ with tab1:
     arr = pd.DataFrame(arr, columns=['Kesulitan','Durasi','Cerita'])
     st.table(arr)
 
-    st.write("0 = Action, 1 = Sport, 2 = Race, 3 = RPG, 4 = FPS, 5 = Simulasi, 6 = Strategi.")
+    st.write("**0 = Action, 1 = Sport, 2 = Race, 3 = RPG, 4 = FPS, 5 = Simulasi, 6 = Strategi.**")
 
     st.write("""
-            Jika sudah menginput klaster yang diinginkan maka klik tombol "Proses" dibawah
+            **Jika sudah menginput klaster yang diinginkan maka klik tombol "Proses" dibawah
             ini untuk memproses k-means dan melihat hasil pengelompokan data sesuai dengan
             klaster. Klaster yang dapat ditentukan hanya sampai 6 klaster karena jika lebih
-            dari 6 klaster proses k-means pada data tidak effisien dalam memberikan hasil.
+            dari 6 klaster proses k-means pada data tidak effisien dalam memberikan hasil.**
             """)
 
     if st.button('Proses'):
         if(nilai == 3):
             st.write("""
-                Tabel dibawah ini merupakan tabel hasil data setelah melalui sekala ulang dan
-                sudah melalui tahap pengelompokan data mining menggunakan k-means. 
+                **Tabel dibawah ini merupakan tabel hasil data setelah melalui sekala ulang dan
+                sudah melalui tahap pengelompokan data mining menggunakan k-means.** 
                 """)
-            st.write("Keterangan :")
-            st.write("0 = Action, 1 = Sport, 2 = Race, 3 = RPG, 4 = FPS, 5 = Simulasi, 6 = Strategi.")
+            st.write("**Keterangan :**")
+            st.write("**0 = Action, 1 = Sport, 2 = Race, 3 = RPG, 4 = FPS, 5 = Simulasi, 6 = Strategi.**")
             scaler = StandardScaler()
             scaler.fit(arr)
             df_scaled = scaler.transform(arr)
@@ -92,8 +92,8 @@ with tab1:
             data_y = pd.DataFrame(data_y, columns=['Nilai Klaster'])
             
             st.write("""
-                    Setelah diproses dengan K-means hasil pengelompokan data sesuai dengan
-                    klaster yang sudah ditentukan seperti pada tabel dibawah ini : 
+                    **Setelah diproses dengan K-means hasil pengelompokan data sesuai dengan
+                    klaster yang sudah ditentukan seperti pada tabel dibawah ini :** 
                 """)
             
             conditions = [
@@ -118,37 +118,37 @@ with tab1:
             data_y = pd.DataFrame(data_y, columns=['Nilai Klaster', 'Klaster', 'Kelompok genre game'])
             st.table(data_y)
             
-            st.write("Keterangan : ")
+            st.write("**Keterangan :** ")
             st.write("""
-                    Nilai Klaster merupakan tabel data dari klaster yang sudah didapatkan,
-                    dikarenakan data merupakan data array maka dimulai dari data ke 0 dan seterusnya.
+                    **Nilai Klaster merupakan tabel data dari klaster yang sudah didapatkan,
+                    dikarenakan data merupakan data array maka dimulai dari data ke 0 dan seterusnya.**
                     """
                     )
             st.write("""
-                    0 = klaster 1, 1 = klaster 2, 2 = klaster 3, dan seterusnya sampai data klaster akhir sesuai input klaster sebelumnya.
+                    **0 = klaster 1, 1 = klaster 2, 2 = klaster 3, dan seterusnya sampai data klaster akhir sesuai input klaster sebelumnya.**
                     """
                     )
-            st.write("C merupakan inisialisasi dari kluster, jadi jika C1 itu merupakan klaster 1 dan seterusnya.")
+            st.write("**C merupakan inisialisasi dari kluster, jadi jika C1 itu merupakan klaster 1 dan seterusnya.**")
         else :
-            st.write(' :red[Untuk data set olah hanya menggunakan 3 klaster, karena sesuai dengan rancangan pembuatan aplikasi ini]')
-            st.write("Silahkan input klaster yang sesuai.")
+            st.write(' :red[**Untuk data set olah hanya menggunakan 3 klaster, karena sesuai dengan rancangan pembuatan aplikasi ini**]')
+            st.write("**Silahkan input klaster yang sesuai.**")
 with tab2:
     def main():
-        st.write("File yang diinput harus bertype csv dan berformat seperti gambar dibawah ini :") 
+        st.write("**File yang diinput harus bertype csv dan berformat seperti gambar dibawah ini :**") 
         st.image("/app/aplikasi_k-means/final/image/datatabel.png", width=500)
-        st.write("Catatan :")
-        st.write("Dengan kolom yang bernama col1, col2, dan col3")        
-        file = st.file_uploader("Upload file", type=["csv"])
+        st.write("**Catatan :")
+        st.write("**Dengan kolom yang bernama col1, col2, dan col3**")        
+        file = st.file_uploader("**Upload file**", type=["csv"])
         show_file = st.empty()
     
         if not file:
-            show_file.info("Please upload a file of type: " + ", ".join(["csv"]))
+            show_file.info("**Please upload a file of type:** " + ", ".join(["csv"]))
             return
     
         content = file.getvalue()
 
         st.write("""
-            Tabel berikut ini merupakan tabel data dari input user untuk dilakukan proses Kmeans.
+            **Tabel berikut ini merupakan tabel data dari input user untuk dilakukan proses Kmeans.**
             """)
         st.write("Dikarenakan data berupa array maka dimulai dari data ke 0, maka data 0 diartikan sebagai data ke 1 dan seterusnya.")
         data = pd.read_csv(file, sep=";", usecols=["col1","col2","col3"])
