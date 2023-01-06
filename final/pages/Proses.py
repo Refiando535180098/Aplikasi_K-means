4796c9b3e3b74f215664b38e5f989731fe8a5f08
Refiando535180098 <refiando.535180098@stu.untar.ac.id>
2023-01-07 00:04:20 +0700

    Update Proses.py

--- final/pages/Proses.py
+++ final/pages/Proses.py
@@ -112,9 +112,6 @@ if selected == "Data set olah":
             data_y['Klaster'] = np.select(conditions, choices)
             data_y = pd.DataFrame(data_y, columns=['Nilai Klaster', 'Klaster'])
             st.table(data_y)
-
-            genre = ['Action', 'Sport', 'Race', 'RPG','FPS', 'Simulasi', 'Strategi']
-            genre = pd.DataFrame(genre, columns=['Genre Games'])
             
             conditions = [
             (data_y['Klaster']=='C1'),
@@ -123,7 +120,11 @@ if selected == "Data set olah":
             choices = ['Tidak disukai','Disukai','Sangat disukai']
             data_y['Kelompok genre game'] = np.select(conditions, choices)
             data_y = pd.DataFrame(data_y, columns=['Nilai Klaster', 'Klaster', 'Kelompok genre game'])
-            st.table(data_y)
+            
+            genre = ['Action', 'Sport', 'Race', 'RPG','FPS', 'Simulasi', 'Strategi']
+            genre = pd.DataFrame(genre, columns=['Genre Games'])
+            
+            st.table(genre, data_y)
             
             st.write("**Keterangan :** ")
             st.write("""
