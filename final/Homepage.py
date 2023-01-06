2256988bebc9febe9848d62a1781917fd5e18838
Refiando535180098 <refiando.535180098@stu.untar.ac.id>
2023-01-06 22:13:51 +0700

    Update Homepage.py

--- final/Homepage.py
+++ final/Homepage.py
@@ -29,7 +29,7 @@ selected = option_menu(
     styles={
         "width": "auto",
         "height": "10px",
-        "nav-link-selected" : {"background-color": "Cornflower Blue"},
+        "nav-link-selected" : {"background-color": "#6495ED"},
     }
 )
 
