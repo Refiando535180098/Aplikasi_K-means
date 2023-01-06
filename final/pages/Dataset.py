bd491a8e702b6cc955f89a591003ee135d70e33d
Refiando535180098 <refiando.535180098@stu.untar.ac.id>
2023-01-06 18:00:23 +0700

    Update Dataset.py

--- final/pages/Dataset.py
+++ final/pages/Dataset.py
@@ -26,10 +26,10 @@ st.markdown(page_bg_img, unsafe_allow_html=True)
 
 st.title("Dataset")
 
-st.write(""" Dataset didapat dari penyebaran kuisioner didapatkan sebanyak
+st.write(""" **Dataset didapat dari penyebaran kuisioner didapatkan sebanyak
          200 data responden dengan dataset berupa data durasi lama bermain, 
          ketertarikan terhadap cerita, dan tingkat kesulitan dari 7 genre game yang umum dimainkan. 
-         seperti action, sport, race, rpg, fps, simulasi, dan strategi.
+         seperti action, sport, race, rpg, fps, simulasi, dan strategi.**
          """)
 
 nama_dataset = st.sidebar.selectbox(
@@ -43,11 +43,11 @@ st.write(f"## Tabel dataset dari : {nama_dataset}")
 
 if (nama_dataset == 'Durasi') :
 
-    st.write(" Berikut ini merupakan tabel dataset durasi lama bermain.")
-    st.write(" Keterangan : nilai pada tabel dalam satuan jam. ")
-    st.write(" 0 : tidak pernah memainkan. ")
-    st.write("5 : dimainkan selama 5 jam atau lebih.")
-    st.write("Dikarenakan data berupa array maka dimulai dari data ke 0, maka data 0 diartikan sebagai data ke 1 dan seterusnya.")
+    st.write(" **Berikut ini merupakan tabel dataset durasi lama bermain.** ")
+    st.write(" **Keterangan : nilai pada tabel dalam satuan jam.** ")
+    st.write(" **0 : tidak pernah memainkan.** ")
+    st.write(" **5 : dimainkan selama 5 jam atau lebih.** ")
+    st.write(" **Dikarenakan data berupa array maka dimulai dari data ke 0, maka data 0 diartikan sebagai data ke 1 dan seterusnya.** ")
 
 
     #query = "SELECT `action`, `sport`, `race`, `rpg`,`fps`, `simulasi`, `strategy` FROM durasi"
@@ -56,15 +56,15 @@ if (nama_dataset == 'Durasi') :
     data1 = pd.read_csv('/app/aplikasi_k-means/final/durasi.csv', sep=";", usecols=[
     "action", "sport", "race", "rpg", "fps", "simulasi", "strategy"])
     array = np.array(data1)
-    array = pd.DataFrame(array, columns=['action', 'sport', 'race', 'rpg','fps', 'simulasi', 'strategi'])
+    array = pd.DataFrame(array, columns=['Action', 'Sport', 'Race', 'RPG','FPS', 'Simulasi', 'Strategi'])
     array
 
 if (nama_dataset == 'Kesulitan') :
-    st.write(" Berikut ini merupakan tabel dataset tingkat kesulitan. ")
-    st.write(" Keterangan : ")
-    st.write(" 1 : Mudah. ")
-    st.write(" 2 : Sedang. ")
-    st.write(" 3 : Sulit. ")
+    st.write(" **Berikut ini merupakan tabel dataset tingkat kesulitan.** ")
+    st.write(" **Keterangan :** ")
+    st.write(" **1 : Mudah.** ")
+    st.write(" **2 : Sedang.** ")
+    st.write(" **3 : Sulit.** ")
     st.write("Dikarenakan data berupa array maka dimulai dari data ke 0, maka data 0 diartikan sebagai data ke 1 dan seterusnya.")
 
 
@@ -74,15 +74,15 @@ if (nama_dataset == 'Kesulitan') :
     data2 = pd.read_csv('/app/aplikasi_k-means/final/kesulitan.csv', sep=";", usecols=[
     "action","sport","race","rpg","fps","simulasi","strategy"])
     array = np.array(data2)
-    array = pd.DataFrame(array, columns=['action', 'sport', 'race', 'rpg','fps', 'simulasi', 'strategi'])
+    array = pd.DataFrame(array, columns=['Action', 'Sport', 'Race', 'RPG','FPS', 'Simulasi', 'Strategi'])
     array
     
 if (nama_dataset == 'Cerita') :
-    st.write(" Berikut ini merupakan tabel dataset ketertarikan cerita. ")
-    st.write(" Keterangan : ")
-    st.write(" 0 : Tidak tertarik. ")
-    st.write(" 6 : Sangat tertarik. ")
-    st.write("Dikarenakan data berupa array maka dimulai dari data ke 0, maka data 0 diartikan sebagai data ke 1 dan seterusnya.")
+    st.write(" **Berikut ini merupakan tabel dataset ketertarikan cerita.** ")
+    st.write(" **Keterangan :** ")
+    st.write(" **0 : Tidak tertarik.** ")
+    st.write(" **6 : Sangat tertarik.** ")
+    st.write(" **Dikarenakan data berupa array maka dimulai dari data ke 0, maka data 0 diartikan sebagai data ke 1 dan seterusnya.** ")
     
     #query = "SELECT `action`, `sport`, `race`, `rpg`,`fps`, `simulasi`, `strategy` FROM cerita"
     #mycursor.execute(query)
@@ -90,5 +90,5 @@ if (nama_dataset == 'Cerita') :
     data3 = pd.read_csv('/app/aplikasi_k-means/final/cerita.csv', sep=";", usecols=[
     "action","sport","race","rpg","fps","simulasi","strategy"])
     array = np.array(data3)
-    array = pd.DataFrame(array, columns=['action', 'sport', 'race', 'rpg','fps', 'simulasi', 'strategi'])
+    array = pd.DataFrame(array, columns=['Action', 'Sport', 'Race', 'RPG','FPS', 'Simulasi', 'Strategi'])
     array
