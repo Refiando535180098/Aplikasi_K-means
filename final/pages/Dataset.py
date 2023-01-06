5ed66717e076921e057bcb501c44f914c3c0d4bd
Refiando535180098 <refiando.535180098@stu.untar.ac.id>
2023-01-06 21:19:44 +0700

    Update Dataset.py

--- final/pages/Dataset.py
+++ final/pages/Dataset.py
@@ -10,7 +10,7 @@ background-image: url("https://img.freepik.com/free-vector/geometric-shape-persp
 background-size: cover;
 }
 [data-testid="stDataFrameResizable"] {
-width: 800px;
+max-width: 800px;
 }
 </style>
 """
