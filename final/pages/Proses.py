6de01d543a84cd9ab0b1629ad32dc81d4d88bac3
Refiando535180098 <refiando.535180098@stu.untar.ac.id>
2023-01-26 19:51:32 +0700

    Update Proses.py

--- final/pages/Proses.py
+++ final/pages/Proses.py
@@ -135,7 +135,7 @@ if selected == "Data set olah":
             st.table(tb_genre)
             
             distortions = []
-            K = range(nilai)
+            K = range(1,3)
             for k in K:
                 kmeanModel = KMeans(n_clusters=k)
                 kmeanModel.fit(data)
